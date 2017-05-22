88034f40075c5ce7c678feda5e6e87214600c9db
yutaka <yutaka.tanoue@gmail.com>
2017-05-22 23:54:03 +0900

    途中段階のものをバックアップ

---
+++ Pythonで金融データを分析しよう/list_01.py
@@ -0,0 +1,42 @@
+import datetime
+import requests
+import pandas as pd
+from datetime import datetime, date
+import zipfile
+
+
+def __gaincap_datetime_parser(str_dt):
+    try:
+        return datetime.strptime(str_dt[:-3], '%Y-%m-%d %H:%M:%S.%f')
+    except Exception:
+        return datetime.strptime(str_dt, '%Y-%m-%d %H:%M:%S')
+
+
+def download_gaincap(filename, path):
+    tsdflg = True
+    url_gc_path = "http://ratedata.gaincapital.com/"
+
+    for i in range(len(filename)):
+        # ファイル取得
+        url_file_path = filename[i]
+        url_date_path = "2017/01 January/"
+        url = url_gc_path + url_date_path + url_file_path
+        res = requests.get(url)
+
+        # ファイルの保存
+        hd_path = path + url_file_path
+
+        # f = open(hd_path, 'wb')
+        # f.write(res.content)
+        # f.close()
+
+        with open(hd_path, 'wb') as f:
+            f.write(res.content)
+
+        # zipファイルからの読み込み
+        file_csv = "USD_JPY_Week%d.csv" %(i+1)
+        try:
+            f = zipfile.ZipFile(hd_path).open(file_csv)
+
+
+
